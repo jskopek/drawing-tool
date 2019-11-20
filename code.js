3717155923b2c3e7bb5d5779550f9f261943b4ca
Jean-Marc Skopek <jskopek@gmail.com>
2019-11-20 01:56:01 -0500

    Only draw when cursor is clicked

--- code.js
+++ code.js
@@ -4,10 +4,25 @@ var context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+var isMousedown = false;
+['touchstart', 'mousedown'].forEach(function (ev) {
+    canvas.addEventListener(ev, function (e) {
+        isMousedown = true
+    });
+});
+
 ['touchmove', 'mousemove'].forEach(function (ev) {
     canvas.addEventListener(ev, function (e) {
+        if (!isMousedown) return
+
         context.lineTo(e.pageX, e.pageY);
         context.stroke()
+    })
+});
+
+['touchend', 'touchleave', 'mouseup'].forEach(function (ev) {
+    canvas.addEventListener(ev, function (e) {
+        isMousedown = false
     })
 });
 
